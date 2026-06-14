6c6d04726ae073329d40ece15a68985995a762fd
Ashwitha Reddy <283939160+donthiashwithareddy@users.noreply.github.com>
2026-06-14 18:08:13 +0530

    main.dart

--- backend/frontend/lib/main.dart
+++ backend/frontend/lib/main.dart
@@ -54,7 +54,7 @@ class _MainLayoutShellState extends State<MainLayoutShell> {
   }
 
   void _initSocket() {
-    socket = io_client.io('https://reentry-envoy-component.ngrok-free.dev', <String, dynamic>{
+    socket = io_client.io('https://queue-cure-26-production.up.railway.app', <String, dynamic>{
       'transports': ['websocket', 'polling'],
       'autoConnect': true,
     });
